ad7a153f85a5f9fd3e00e643104f8d04b67dac63
Adrian <adriansurdu33@gmail.com>
2022-09-26 17:41:12 +0300

    Added game script

---
+++ script.js
@@ -0,0 +1,43 @@
+let availableChoices = ["rock","paper","scissors"];
+
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function getComputerChoice(){
+    return availableChoices[randomIntFromInterval(0,2)];
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    switch (true) {
+        case playerSelection==="rock" && computerSelection==="paper":
+            return "You Lose! Paper beats Rock";
+            break;
+        case playerSelection==="rock" && computerSelection==="scissors":
+            return "You Win! Rock beats Scissors";
+            break;
+        case playerSelection==="paper" && computerSelection==="rock":
+            return "You Win! Paper beats Rock";
+            break;
+        case playerSelection==="paper" && computerSelection==="scissors":
+            return "You Lose! Scissors beats Paper";
+            break;
+        case playerSelection==="scissors" && computerSelection==="rock":
+            return "You Lose! Rock beats Scissors";
+            break;
+        case playerSelection==="scissors" && computerSelection==="paper":
+            return "You Win! Scissors beats Paper";
+            break;
+        default:
+            return "Tie!";
+            break;
+    }
+}
+
+for(let i=0;i<5;i++) {
+    const playerSelection = prompt();
+    const computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+}
