07da1a27928b9749b9492efb7a410d0933932280
Adrian <adriansurdu33@gmail.com>
2022-11-07 22:14:30 +0200

    Added Home Page

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-let availableChoices = ["rock","paper","scissors"];
+let availableChoices = ["rock", "paper", "scissors"];
 
 function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function getComputerChoice(){
-    return availableChoices[randomIntFromInterval(0,2)];
+function getComputerChoice() {
+    return availableChoices[randomIntFromInterval(0, 2)];
 }
-
 
 function playRound(playerSelection, computerSelection) {
     switch (true) {
-        case playerSelection==="rock" && computerSelection==="paper":
+        case playerSelection === "rock" && computerSelection === "paper":
             return "You Lose! Paper beats Rock";
             break;
-        case playerSelection==="rock" && computerSelection==="scissors":
+        case playerSelection === "rock" && computerSelection === "scissors":
             return "You Win! Rock beats Scissors";
             break;
-        case playerSelection==="paper" && computerSelection==="rock":
+        case playerSelection === "paper" && computerSelection === "rock":
             return "You Win! Paper beats Rock";
             break;
-        case playerSelection==="paper" && computerSelection==="scissors":
+        case playerSelection === "paper" && computerSelection === "scissors":
             return "You Lose! Scissors beats Paper";
             break;
-        case playerSelection==="scissors" && computerSelection==="rock":
+        case playerSelection === "scissors" && computerSelection === "rock":
             return "You Lose! Rock beats Scissors";
             break;
-        case playerSelection==="scissors" && computerSelection==="paper":
+        case playerSelection === "scissors" && computerSelection === "paper":
             return "You Win! Scissors beats Paper";
             break;
         default:
@@ -35,7 +34,7 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-for(let i=0;i<5;i++) {
+for (let i = 0; i < 0; i++) {
     const playerSelection = prompt();
     const computerSelection = getComputerChoice();
     console.log(computerSelection);
